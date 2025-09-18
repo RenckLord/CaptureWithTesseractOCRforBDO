30b4b26e8fb43b9d9799624b016ac2ae4f42e7f5
Irving <irvingg.60.77@gmail.com>
2025-09-17 19:13:33 -0600

    irving/Ajuste de Comentarios

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -1,25 +1,22 @@
-// frontend/public/electron.js
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  // Configuración de la ventana del overlay
   const win = new BrowserWindow({
-    width: 350,       // Ancho del overlay
-    height: 250,      // Alto del overlay
+    width: 350,       // Ancho 
+    height: 250,      // Alto 
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
-    transparent: true, // Habilita la transparencia
-    frame: false,      // Oculta la barra de título y bordes
-    alwaysOnTop: true, // Mantiene la ventana siempre visible
+    transparent: true, 
+    frame: false,      
+    alwaysOnTop: true, 
     resizable: false,
     movable: true,
-    skipTaskbar: true, // No la muestra en la barra de tareas de Windows
+    skipTaskbar: true, 
   });
   
-  // Hace que los clics "atraviesen" la ventana, para que puedas seguir jugando
   //win.setIgnoreMouseEvents(true);
 
   // Carga la aplicación de React
